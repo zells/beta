5c3885cdfe0280c5250af306442112b7208a03f6
Nikolas Martens <Nikolas.M@rtens.org>
2015-03-29 14:47:37 +0200

    Specified how cell changes are observed

---
+++ test/src/org/rtens/zells/beta/ObserveChanges.java
@@ -0,0 +1,72 @@
+package org.rtens.zells.beta;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * In order to update the UI, changes in cell can be observed.
+ */
+public class ObserveChanges {
+
+    @Test
+    @Ignore
+    public void _FailIfCellDoesNotExist() {
+//        whenIObserve("foo");
+//        thenItShouldThrowAnException("[foo] does not exist.");
+    }
+
+    @Test
+    @Ignore
+    public void _NotifyAboutNewChild() {
+//        givenACell("foo");
+//        givenIAmObserving("foo");
+//        whenICreate_Under("bar", "foo");
+//        thenIShouldBeNotifiedAboutNewCell("foo/bar");
+    }
+
+    @Test
+    @Ignore
+    public void _NotifyAboutNewGrandChild() {
+//        givenACell("foo/bar");
+//        givenIAmObserving("foo");
+//        whenICreate_Under("baz", "foo/bar");
+//        thenIShouldNeNotifiedAboutThe_Cell("created", "foo/bar/baz");
+    }
+
+    @Test
+    @Ignore
+    public void _NotifyAboutDeletedCell() {
+//        givenACell("foo/bar/baz");
+//        givenIAmObserving("foo");
+//        whenIDelete("foo/bar/baz");
+//        thenIShouldBeNotifiedAboutThe_Cell("deleted", "foo/bar/baz");
+    }
+
+    @Test
+    @Ignore
+    public void _NotifyAboutChangedStem() {
+//        givenACell("foo/bar");
+//        givenIAmObserving("foo");
+//        whenIChangeTheStemOf_To("foo/bar", "new/stem");
+//        thenIShouldBeNotifiedAboutThe_Cell("changedStem", "foo/bar");
+    }
+
+    @Test
+    @Ignore
+    public void _NotifyAboutChangedResponse() {
+//        givenACell("foo/bar");
+//        givenIAmObserving("foo");
+//        whenIChangeTheResponseOf("foo/bar");
+//        thenIShouldBeNotifiedAboutThe_Cell("changedResponse", "foo/bar");
+    }
+
+    @Test
+    @Ignore
+    public void _NotifyAboutChangedName() {
+//        givenACell("foo/bar");
+//        givenIAmObserving("foo");
+//        whenIChangeTheNameOf_To("foo/bar", "baz");
+//        thenIShouldBeNotifiedAboutThe_Cell("deleted", "foo/bar");
+//        thenIShouldBeNotifiedAboutThe_Cell("created", "foo/baz");
+    }
+}
